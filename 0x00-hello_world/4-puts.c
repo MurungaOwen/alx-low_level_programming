6fc9bbfa6ca59b9bff43428d76c02e0e522604d1
MurungaOwen <owenhood80@gmail.com>
2023-02-19 17:14:32 -0800

    correction pof 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
+/*
+ * program thet uses puts as a function*/
 int main(void){
-	/*
-	 *program that uses puts instead of printf
-	 * */
-puts("/"Programming is like building a multilingual puzzle \n");
+puts("/"Programming is like building a multilingual puzzle");
 return (0);
 }
 
