0144a5f287d23e8f8ec06301948b74dd426e1b6f
MurungaOwen <owenhood80@gmail.com>
2023-02-19 17:44:01 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main-a program that uses printf to display characters
+ * description-using printf instead of puts
+ * Return:0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
