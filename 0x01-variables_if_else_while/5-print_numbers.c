619d42c2ad9dcbb0d37284baf09989ea7a2fae19
MurungaOwen <owenhood80@gmail.com>
2023-02-21 16:58:45 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-function that displays single base 10 integers
+ * Return:0(Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while(i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
