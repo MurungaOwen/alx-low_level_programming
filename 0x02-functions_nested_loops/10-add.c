a16939ba8982406bbc715f6793c71f4cf7cff695
MurungaOwen <owenhood80@gmail.com>
2023-02-23 05:46:18 -0800

    task 10..add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add-adds two numbers
+ *Return:no return value
+ *@i:integer1
+ *@j:integer2
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	_putchar(k + 48);
+	_putchar('\n');
+}
