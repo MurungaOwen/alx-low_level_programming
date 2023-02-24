e163fd5891cff6f15a38099fc5d1773c406e6961
MurungaOwen <owenhood80@gmail.com>
2023-02-24 00:45:42 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,30 +7,29 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
-	if (i <= 98)
+	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
 			if(i != 98)
 				printf("%d, ", i);
+			else if (i == 98)
 			printf("%d\n", i);
-
-
 		}
 	}
-	else if(i >= 98)
+	else if (n >= 98)
 	{
-		for (i = n; i > 98; i--)
+		for (j = n; j >= 98; j--)
 		{
-			if (i == 98)
+			if (j != 98)
 			{
-				printf("98\n");
+				printf("%d, ", j);
 			}
-			else if (i > 98)
+			else if (j == 98)
 			{
-				printf("%d\n",i);
+				printf("%d\n",j);
 			}
 		}
 	{	
