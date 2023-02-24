9efb07311f5085d8a400966ebf9f138e8010994b
MurungaOwen <owenhood80@gmail.com>
2023-02-23 22:33:41 -0800

    task1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isupper-a function that checks for upper case letters
+ * Return: 1 if upper 0 othrerwise
+ * @c:ASCII code
+ */
+int _isupper(int c)
+{
+	if (c >= 65 &&  c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
