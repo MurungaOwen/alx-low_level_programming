f03bab1bb2a4db9c253afeee28fba77be6b36f25
MurungaOwen <owenhood80@gmail.com>
2023-02-27 18:20:28 -0800

    prnt numbers with exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers-a function that prints numbers except 2 and 4
+ * Return:no return value
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a == 50 || a ==52)
+			continue;
+		_putchar(a);
+	}
+	_putchar('\n');
+}
