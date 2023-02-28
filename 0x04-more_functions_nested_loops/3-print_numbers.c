dc9a1fa082b4f36aaaaad11a837ef64d67d9d08f
MurungaOwen <owenhood80@gmail.com>
2023-02-27 18:09:00 -0800

    task on printing numbers followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers-a function to display digits
+ * Return:no return value
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
