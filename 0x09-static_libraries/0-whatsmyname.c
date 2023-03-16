2a5094910c29d7cbecbe5276ee06f5059117165a
MurungaOwen <owenhood80@gmail.com>
2023-03-16 05:21:15 -0700

    task 0

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main-a function that calls itself
+ *@argc:the number of argument
+ *@*argv[]:the array of values in v
+ * return:0 success
+ */
+int main(int argc ,char *argv[])
+{
+
+	printf("%s",*argv[0]);
+	printf("\n");
+	return (0)
+}
